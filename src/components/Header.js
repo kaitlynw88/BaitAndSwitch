c8a9d675b99eea094c2d28876c6c82351bb4c047
KaitlynWickson <kwickson28@gmail.com>
2025-03-11 19:50:38 -0400

    error handling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
